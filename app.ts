428677e6eeb2513df16a3a891b713b5c2fd7700b
boen <workingxw@hotmail.com>
2025-06-20 16:34:12 +0800

    feat:  替换模版引擎

--- app.ts
+++ app.ts
@@ -5,43 +5,43 @@ addAliases({
   '@config': `${__dirname}/config`,
   '@middlewares': `${__dirname}/middlewares`,
 });
+
 import Koa from 'koa';
 import { createContainer, Lifetime } from 'awilix';
-import co from 'co';
-import render from 'koa-swig';
+import views from 'koa-views'; // 替换 koa-swig
 import config from '@config/index';
 import serve from 'koa-static';
 import { loadControllers, scopePerRequest } from 'awilix-koa';
-//koa中没有实现的路由重定向到index.html
 import { historyApiFallback } from 'koa2-connect-history-api-fallback';
+
 const app = new Koa();
-const { port, viewDir, memoryFlag, staticDir } = config;
-//静态资源生效节点
+const { port, viewDir, staticDir } = config;
+
+// 静态资源
 app.use(serve(staticDir));
+
 const container = createContainer();
 
-//所有的可以被注入的代码都在container中
+// 依赖注入
 container.loadModules([`${__dirname}/services/*.ts`], {
   formatName: 'camelCase',
   resolverOptions: {
     lifetime: Lifetime.SCOPED,
   },
 });
-//每一次用户请求router中 都会从容器中取到注入的服务
+
 app.use(scopePerRequest(container));
-app.context.render = co.wrap(
-  render({
-    root: viewDir,
-    autoescape: true,
-    cache: <'memory' | false>memoryFlag,
-    writeBody: false,
-    ext: 'html',
-  })
-);
+
+// 替换模板引擎配置
+app.use(views(viewDir, {
+  extension: 'html',
+  map: { html: 'ejs' } // 使用ejs渲染html文件
+}));
+
 app.use(historyApiFallback({ index: '/', whiteList: ['/api'] }));
-//让所有的路由全部生效
 app.use(loadControllers(`${__dirname}/routers/*.ts`));
+
 app.listen(port, () => {
   console.log('BFF启动成功');
-  console.log('network:',`127.0.0.1:${port}`)
+  console.log('network:', `127.0.0.1:${port}`);
 });
